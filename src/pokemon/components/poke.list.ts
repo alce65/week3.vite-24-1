520ebe619d46240d1ecf5a32eb64c45c0a37832d
alce65 <alce65@hotmail.es>
2024-03-17 20:08:47 +0100

    Use repo in poke list

--- src/pokemon/components/poke.list.ts
+++ src/pokemon/components/poke.list.ts
@@ -3,24 +3,26 @@
 import { Component } from '../../components/component';
 import { Pokemon } from '../models/pokemon';
 import { PokemonApiRepo } from '../repo/poke.api.repo';
+import { StateStructure } from '../types/state';
 import { PokeItem } from './poke.item';
 
 export class PokeList extends Component {
+  state: StateStructure | null = null;
   constructor(
     public selector: string,
-    private pokeData: Array<Pokemon>,
     private repo: PokemonApiRepo // ALT private state: any // State
   ) {
     super(selector);
-    this.repo.getAllPokemons();
-    this.render();
+    this.repo.getAllPokemons().then((data) => {
+      this.state = data;
+      this.render();
+    });
   }
 
   render() {
     this.template = this.createTemplate();
-    this.element = document.querySelector(this.selector) as HTMLElement;
     super.render();
-    this.pokeData.forEach(
+    this.state?.pokeData.forEach(
       (poke: Pokemon) => new PokeItem('.poke-list__list', poke)
     );
 
@@ -29,11 +31,21 @@ export class PokeList extends Component {
         const target = e.target as HTMLElement;
         const { id } = target.dataset;
         if (id === 'prev') {
-          // Future this.state.prev();
           console.log('prev');
+          if (!this.state?.previousUrl) return;
+          this.repo.getAllPokemons(this.state?.previousUrl).then((data) => {
+            this.state = data;
+            this.element.remove();
+            this.render();
+          });
         } else {
-          // Future this.state.next();
           console.log('next');
+          if (!this.state?.nextUrl) return;
+          this.repo.getAllPokemons(this.state?.nextUrl).then((data) => {
+            this.state = data;
+            this.element.remove();
+            this.render();
+          });
         }
       });
     });
@@ -57,30 +69,29 @@ export class PokeList extends Component {
   // }
 
   createTemplate() {
-    // Alt const final = this.state.nextUrl
-    //   ? this.state.nextUrl.split('=')[1].split('&')[0]
-    //   : this.state.count;
-    // const initial = +final - 19;
-    // const paginationText = `${initial} - ${final} / ${this.state.count}`;
-    //
-    // const template = `
-    //         <h2>Lista de Pokemons (${paginationText})</h2>
-    //         <ul class="poke-list__list"></ul>
-    //         <div class="pagination"></div>`;
+    const final = this.state?.nextUrl
+      ? this.state.nextUrl.split('=')[1].split('&')[0]
+      : this.state?.count;
+    const initial = Number(final) - 19;
+    const paginationText = `${initial} - ${final} / ${this.state?.count}`;
 
     const template = `
-      <h2>Lista de Pokemons </h2>
+    <section class="poke-container">
+      <h2>Lista de Pokemons (${paginationText})</h2>
       <ul class="poke-list__list"></ul>
       <div class="pagination">
-         <button class="pagination__button isHidden" type="button" data-id="prev">
+         <button class="pagination__button 
+         ${this.state?.previousUrl || 'isHidden'}" type="button" data-id="prev">
               <i class="fas fa-backward" data-id="prev"></i>
               <span data-id="prev">Anterior</span>
           </button>
-          <button class="pagination__button" type="button" data-id="next">
+          <button class="pagination__button 
+           ${this.state?.nextUrl || 'isHidden'}" type="button" data-id="next">
               <span data-id="next">Siguiente</span>
               <i class="fas fa-forward" data-id="next"></i>
           </button>
       </div>
+    </section>
     `;
     return template;
   }
